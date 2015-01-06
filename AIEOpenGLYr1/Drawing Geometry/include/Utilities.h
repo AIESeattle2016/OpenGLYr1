14ab118858532de612284ccdec1a87997d13e67a
Terry Nguyen <terreh@terrehbyte.com>
2015-01-06 14:49:55 -0800

    Drawing triangle to screen

--- AIEOpenGLYr1/Drawing Geometry/include/Utilities.h
+++ AIEOpenGLYr1/Drawing Geometry/include/Utilities.h
@@ -20,4 +20,6 @@
 GLuint CreateShader(GLenum a_eShaderType, const char *a_strShaderFile);
 GLuint CreateProgram(const char *a_vertex, const char *a_frag);
 
+float* getOrtho(float left, float right, float bottom, float top, float a_fNear, float a_fFar);
+
 #endif
--- AIEOpenGLYr1/Drawing Geometry/main.cpp
+++ AIEOpenGLYr1/Drawing Geometry/main.cpp
@@ -11,6 +11,8 @@
 
 #include <GLFW\glfw3.h>	// GLFW
 
+#include <Utilities.h>	// CreateProgram...
+
 int main(int argc, char * argv[])
 {
 	// # WINDOW CREATION
@@ -23,7 +25,7 @@ int main(int argc, char * argv[])
 
 	// Create a windowed mode window and its OpenGL context
 	GLFWwindow * window;
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1024, 720, "Hello World", NULL, NULL);	// @AIE - the screen resolution has changed from the previous tutorial?
 	
 	// Verify window creation
 	if (!window)
@@ -43,10 +45,54 @@ int main(int argc, char * argv[])
 
 	// # GAME LOGIC
 
+	// ## Triangle Data
+	const float vertexPositions[] =
+	{
+		1024 / 2.0, 720 / 2.0 + 10.0f, 0.0f, 1.0f,
+		1024 / 2.0 - 5.0f, 720 / 2.0 - 10.0f, 0.0f, 1.0f,	//@AIE - typo on this line! there was a duplicate "f"!
+		1024 / 2.0 + 5.0f, 720 / 2.0 - 10.0f, 0.0f, 1.0f,
+	};
+
+	const float vertexColours[] =
+	{
+		1.0f, 0.0f, 0.0f, 1.0f,
+		0.0f, 1.0f, 0.0f, 1.0f,
+		0.0f, 0.0f, 1.0f, 1.0f,
+	};
+
+	// ## Triangle Shaders
+	GLuint uiProgramFlat = CreateProgram("resources/shaders/VertexShader.glsl", "resources/shaders/FlatFragmentShader.glsl");
+
+	GLuint MatrixIDFlat = glGetUniformLocation(uiProgramFlat, "MVP");	// get handle to MVP uniform in shader
+
+	float * orthographicProjection = getOrtho(0, 1024, 0, 720, 0, 100);
+
+
 	// loop until window is closed
 	while (!glfwWindowShouldClose(window))
 	{
 		// Draw code goes here
+
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);	// color of empty pixel
+		glClear(GL_COLOR_BUFFER_BIT);			// clear using the specified method
+
+		//enable shaders
+		glUseProgram(uiProgramFlat);
+
+		//send our orthographic projection info to the shader
+		glUniformMatrix4fv(MatrixIDFlat, 1, GL_FALSE, orthographicProjection);
+
+		//enable the vertex array state, since we're sending in an array of vertices
+		glEnableVertexAttribArray(0);
+		glEnableVertexAttribArray(1);
+
+		//specify where our vertex array is, how many components each vertex has, 
+		//the data type of each component and whether the data is normalised or not
+		glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, vertexPositions);
+		glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, vertexColours);
+
+		//draw to the screen
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		// swap front and back buffers
 		glfwSwapBuffers(window);
