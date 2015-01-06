ce79d7a65481a5a10c47a3f81a9ff4127245a68d
Terry Nguyen <terreh@terrehbyte.com>
2015-01-06 14:13:58 -0800

    Statically linked GLFW3 and GLEW 1.11.0

--- AIEOpenGLYr1/Drawing Geometry/main.cpp
+++ AIEOpenGLYr1/Drawing Geometry/main.cpp
@@ -6,7 +6,43 @@
 //
 //////////////////////////////////////////////////////////////////////////
 
+#include <GLFW\glfw3.h>
+
 int main(int argc, char * argv[])
 {
+	// Initialize GLFW
+	if (!glfwInit())
+	{
+		// Failed to initialize GLFW, return -1 for error
+		return -1;
+	}
 
+	// Create a windowed mode window and its OpenGL context
+	GLFWwindow * window;
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	
+	// Verify window creation
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	// loop until window is closed
+	while (!glfwWindowShouldClose(window))
+	{
+		// Draw code goes here
+
+		// swap front and back buffers
+		glfwSwapBuffers(window);
+
+		// poll for and process events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
